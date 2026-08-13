70674b44910fc97dc17bb03c75d515f1afdac21a
Marmiril <angel.plata@outlook.es>
2026-08-13 11:01:56 +0200

    Ex40 exceptions

---
+++ src/main/java/com/angel/springbootlearning/exercises/exercise40/exception/DuplicateStudentNameException40.java
@@ -0,0 +1,11 @@
+/*
+ * Exercise 40 - Architecture Review
+ * Purpose: Represent an error caused by a duplicated student name.
+ * URL: http://localhost:8080/ex40/students
+ */
+
+package com.angel.springbootlearning.exercises.exercise40.exception;
+
+public class DuplicateStudentNameException {
+    
+}
